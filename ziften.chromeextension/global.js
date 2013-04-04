6909d1aba88b7917a868f35403928e5e12fcb06c
Mathijs Kadijk <mathijs@wrep.nl>
2013-04-04 22:50:40 +0200

    Fix for Chrome extension where not all settings where initialized correctly

--- ziften.chromeextension/global.js
+++ ziften.chromeextension/global.js
@@ -18,14 +18,11 @@ var ziftenGlobal = (function() {
 	};
 
 	// Initialize settings if needed
-	if ( !('ziftenInitialized' in localStorage) ) {
-		localStorage.othersIssues = localStorage.searchfieldJumpToIssue = localStorage.searchfieldJumpToProject = 1;
+	if (undefined === localStorage.ziftenSettingsVersion || 1 > localStorage.ziftenSettingsVersion) {
+		localStorage.othersIssues = localStorage.searchfieldJumpToIssue = localStorage.searchfieldJumpToProject = localStorage.selectIssueNumberOnClick = localStorage.asyncFiltering = 1;
 		localStorage.hotkeys = localStorage.mentionIssues = 2;
-		localStorage.ziftenInitialized = 1;
-	}
-	if (2 > localStorage.ziftenInitialized) {
-		localStorage.selectIssueNumberOnClick = localStorage.asyncFiltering = 1;
-		localStorage.ziftenInitialized = 2;
+
+		localStorage.ziftenSettingsVersion = 1;
 	}
 
 	// Install message handler
